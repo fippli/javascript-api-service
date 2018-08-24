b685db0e88fa750655011c7a23bce53374dd40f4
fippli <filip.carl.johansson@gmail.com>
2018-08-24 15:45:35 -0500

    Added GET, POST, PUT, DELETE Added tests with jest

---
+++ index.js
@@ -0,0 +1,92 @@
+/*
+JavaScript API utilities.
+Make http requests.
+ */
+
+// ----------------------------------------------------------------------------------------------------
+// Handle options
+
+const defaultHeaders = { "Content-type": "application/json" };
+const createHeaders = ( headersObject = {} ) => {
+    return Object.assign( {}, defaultHeaders, headersObject )
+};
+
+const defaultOptions = {};
+const buildUrl = ( endpoint, options = defaultOptions ) => {
+    // Port is required for localhost
+    if ( options.host === "localhost" && !options.port ) {
+        throw new Error( `You need to specify a port when host is set to "localhost".` )
+    }
+    return `${ options.host ? `http://${options.host}:${options.port ? options.port : ""}` : ""}${endpoint}`
+};
+
+// ----------------------------------------------------------------------------------------------------
+// Handle response
+
+const verifyResponse = response => {
+    const contentType = response.headers.get( "content-type" );
+    if ( contentType && contentType.indexOf( "application/json" ) !== -1 ) {
+        return response.json();
+    } else {
+        handleError( "Response was not JSON" );
+    }
+};
+
+const handleError = error => {
+    const errorMessage = `
+        Error [@fippli/api-utils]:
+        API request failed: 
+        ${error}
+    `;
+    throw new Error( errorMessage );
+};
+
+// ----------------------------------------------------------------------------------------------------
+// METHODS
+
+const GET = ( endpoint = "/", options = defaultOptions ) => {
+    return fetch( buildUrl( endpoint, options ), {
+        method: "GET",
+        headers: createHeaders( options.headers )
+    })
+    .then( verifyResponse, handleError );
+};
+
+const PUT = ( endpoint, data, options = defaultOptions ) => {
+    return fetch( buildUrl( endpoint, options ), {
+        method: "PUT",
+        headers: createHeaders( options.headers ),
+        body: JSON.stringify( data )
+    })
+    .then( verifyResponse, handleError );
+};
+
+const POST = ( endpoint, data, options = defaultOptions ) => {
+    return fetch( buildUrl( endpoint, options ), {
+        method: "POST",
+        headers: createHeaders( options.headers ),
+        body: JSON.stringify( data )
+    })
+    .then( verifyResponse, handleError );
+};
+
+const DELETE = ( endpoint, data, options = defaultOptions ) => {
+    return fetch( buildUrl( endpoint, options ), {
+        method: "DELETE",
+        headers: createHeaders( options.headers ),
+        body: JSON.stringify( data )
+    })
+    .then( verifyResponse, handleError );
+};
+
+// ----------------------------------------------------------------------------------------------------
+// Module exports
+
+module.exports = {
+    GET: GET,
+    PUT: PUT,
+    POST: POST,
+    DELETE: DELETE,
+    buildUrl: buildUrl,
+    createHeaders: createHeaders,
+};
---
+++ test.js
@@ -0,0 +1,38 @@
+const apiUtils = require("./index");
+
+//
+// Test build url
+//
+test('Build url with endpoint "/".', () => {
+    expect(apiUtils.buildUrl("/")).toBe("/");
+});
+
+test('Build url with endpoint "/" and options {host:"localhost", port:3000}.', () => {
+    expect(apiUtils.buildUrl("/", {host:"localhost", port:3000})).toBe("http://localhost:3000/");
+});
+
+test('Build url with endpoint "/api/v1/test".', () => {
+    expect(apiUtils.buildUrl("/api/v1/test")).toBe("/api/v1/test");
+});
+
+test('Build url with endpoint "/api/v1/test" and options {host:"localhost", port:8000}.', () => {
+    expect(apiUtils.buildUrl("/api/v1/test", {host:"localhost", port:8000})).toBe("http://localhost:8000/api/v1/test");
+});
+
+//
+// Test create headers
+//
+test('Create headers without parameters.', () => {
+    expect(apiUtils.createHeaders()).toEqual(
+        expect.objectContaining({"Content-type":"application/json"})
+    )
+});
+
+test('Create headers with parameter {"test-header": "test-value"}.', () => {
+    expect(apiUtils.createHeaders({"test-header": "test-value"})).toEqual(
+        expect.objectContaining({
+            "Content-type":"application/json",
+            "test-header":"test-value"
+        })
+    )
+});
